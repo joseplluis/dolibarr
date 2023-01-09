d038e0281c8cb78362f84cb0819254adb3a01d9a
joseplluis <joseplluis@users.noreply.github.com>
2023-01-09 15:25:24 +0000

    Deploying to gh-pages from @ joseplluis/dolibarr@c973b7d59c0b20c3b4625b63eccead5bd2d743cb 🚀

--- search_config.php
+++ search_config.php
@@ -16,7 +16,7 @@ $translator = array(
   'search_matches' => "Matches:",
   'search' => "Search",
   'split_bar' => "",
-  'logo' => "Generated on Sun Jan 8 2023 15:16:40 for dolibarr by&#160;\n<a href=\"https://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.svg\" width=\"104\" height=\"31\" alt=\"doxygen\"/></a> 1.9.1 ",
+  'logo' => "Generated on Mon Jan 9 2023 15:24:39 for dolibarr by&#160;\n<a href=\"https://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.svg\" width=\"104\" height=\"31\" alt=\"doxygen\"/></a> 1.9.1 ",
 );
 
 ?>
